2f7be7c9c1dbc1cbcef5d9cc4e916e317711a9b8
Manuel Torres <manueljosetorres02@gmail.com>
2020-09-27 22:56:51 -0500

    Updates

---
+++ precache-manifest.1688752e20ce443f98337f62a8f2e235.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc636bfc635fd942e3db2872eadeedc0",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "2787b4c4bf446121906a",
+    "url": "/my_portfolio/static/css/main.53c398e7.chunk.css"
+  },
+  {
+    "revision": "25b45025c62a0659d90e",
+    "url": "/my_portfolio/static/js/2.8aa77f72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_portfolio/static/js/2.8aa77f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2787b4c4bf446121906a",
+    "url": "/my_portfolio/static/js/main.32d4dd54.chunk.js"
+  },
+  {
+    "revision": "d9629bd1ee9955b69a66",
+    "url": "/my_portfolio/static/js/runtime-main.88b1f98b.js"
+  },
+  {
+    "revision": "b373cdf1fe5fcbea589fd18e36c156fc",
+    "url": "/my_portfolio/static/media/cropped.b373cdf1.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
